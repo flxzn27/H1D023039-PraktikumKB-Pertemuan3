aeb0b19da6adefc3a21aa49f900ce5adca818ec9
Alfan Fauzan <131223601+flxzn27@users.noreply.github.com>
2025-03-22 10:02:00 +0700

    Update lavida.py

--- lavida.py
+++ lavida.py
@@ -4,18 +4,16 @@ import numpy as np
 print("===== SELAMAT DATANG DI TOKO LA VIDA =====")
 
 # Struktur Data
-tuple_jenis = ["Halli Galli", "Yut Nori", "Gonggi", "Ddakji"]
-list_harga = [65000, 25000, 10000, 15000]
+tuple_jenis = np.array(["Halli Galli", "Yut Nori", "Gonggi", "Ddakji"])
+list_harga = np.array([65000, 25000, 10000, 15000])
 set_bonus = {"Permen Lolipop", "Tempat Pensil", "Penghapus"}
 
-# Menampilkan daftar harga
 print("\n== LIST HARGA ==")
 for i in range(len(tuple_jenis)):
     print(f"{i+1}. {tuple_jenis[i]} - Rp {list_harga[i]:,}")
 
-# Input jumlah item yang dibeli
 total_harga = 0
-keranjang = [0] * len(tuple_jenis)
+keranjang = np.zeros(len(tuple_jenis), int)
 while True:
     pilihan = input("Masukkan nomor produk yang ingin dibeli (atau ketik 'selesai' untuk checkout): ")
     if pilihan.lower() == 'selesai':
@@ -28,19 +26,20 @@ while True:
     index = int(pilihan) - 1
     jumlah = input(f"Masukkan jumlah {tuple_jenis[index]} yang ingin dibeli: ")
     
-    keranjang[index] += int(jumlah)
+    if not jumlah.isnumeric() or int(jumlah) <= 0:
+        print("Jumlah harus angka positif!")
+        continue
     
-    print(f"{tuple_jenis[index]} x{jumlah} ditambahkan ke keranjang. Total sementara: Rp {sum(keranjang[i] * list_harga[i] for i in range(len(tuple_jenis))):,}")
+    keranjang[index] += int(jumlah)
+    print(f"{tuple_jenis[index]} x{jumlah} ditambahkan ke keranjang. Total sementara: Rp {sum(keranjang * list_harga):,}")
 
-# Menampilkan struk belanja
-total_harga = sum(keranjang[i] * list_harga[i] for i in range(len(tuple_jenis)))
+total_harga = sum(keranjang * list_harga)
 print("\n===== STRUK BELANJA =====")
 for i in range(len(keranjang)):
     if keranjang[i] > 0:
         print(f"{tuple_jenis[i]} x{keranjang[i]}")
 print(f"Total Harga: Rp {total_harga:,}")
 
-# Menentukan bonus jika total harga lebih dari 40.000
 if total_harga > 40000:
     bonus_acak = random.choice(list(set_bonus))
     print(f"Selamat! Anda mendapatkan bonus: {bonus_acak}")
